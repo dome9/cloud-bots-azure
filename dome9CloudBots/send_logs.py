e4030660c467606f222276f524ff8a9faa6b01bb
Ravid Atias <ravida@checkpoint.com>
2019-07-01 09:57:15 +0300

    remove report time from sumo log and add logging info

--- dome9CloudBots/send_logs.py
+++ dome9CloudBots/send_logs.py
@@ -7,15 +7,21 @@ from time import gmtime, strftime
 SUMO_HTTP_ENDPOINT = 'https://endpoint4.collection.us2.sumologic.com/receiver/v1/http/ZaVnC4dhaV3N-lPxodY4J3xadi2by444XVaSboLlcbfMeGhqAnZn4PIVuJw_h3EzhhCv4jEFLfhHO3nbvfVgVSiRrB2X1hedSvXwyKB31hF3zdmR7j7mrQ=='
 
 def send_logs(message, start_time, vendor):
-    logging.info(f'{__file__} - start send logs')  
+    logging.info(f'{__file__} - start send logs')
     execution_time = time.time() - start_time
     session = requests.Session()
+    account_id = message.get('Account id')
+    logging.info(f'{__file__} - vendor: {vendor}')
+    logging.info(f'{__file__} - message: {message}')
+    del message['ReportTime']
     for bot in message.get('Rules violations found'):
         del bot['ID']
         del bot['Name']
-    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'X-Sumo-Name': message.get('Account id'), 'X-Sumo-Category': vendor}
+    headers = {'Content-Type': 'application/json',  'Accept': 'application/json', 'X-Sumo-Name': account_id, 'X-Sumo-Category': vendor}
     data = {'msg': message,
             'execution_time': execution_time}
+    logging.info(f'{__file__} - data: {data}')
     return_sumo_status = session.post(SUMO_HTTP_ENDPOINT, headers=headers, data=json.dumps(data))
     logging.info(f'{__file__} - status code from dome9 logs: {return_sumo_status.status_code}')
     return
+
