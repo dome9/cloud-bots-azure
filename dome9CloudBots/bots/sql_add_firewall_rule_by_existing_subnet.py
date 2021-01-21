b94663d3099a9ec3f19881f29df91f045462a67f
chrisbeckett <chris@chrisbeckett.org.uk>
2021-01-21 14:27:02 +0000

    New bots - Jan 21

---
+++ dome9CloudBots/bots/sql_add_firewall_rule_by_existing_subnet.py
@@ -0,0 +1,60 @@
+# What it does: Sets a firewall rule on Azure SQL Server based on EXISTING subnets in EXISTING VNets
+# Usage: sql_add_firewall_rule_by_existing_subnet - <firewall rule name> <vnet-resource-group> <existing-vnet-name> <existing-subnet-name>
+# Example: sql_add_firewall_rule_by_existing_subnet my_vnet_rule my-resource-group vnet-example subnet-example
+# Limitations: A valid VNet SQL Service Endpoint MUST already exist, or the bot will fail
+# Last checked 21/1/21
+
+import logging
+from azure.core.exceptions import HttpResponseError
+from azure.mgmt.sql import SqlManagementClient
+from azure.mgmt.sql.models import Server
+
+
+def raise_credentials_error():
+    msg = 'Error! Subscription id or credentials are missing.'
+    logging.info(f'{__file__} - {msg}')
+    return msg
+
+def run_action(credentials, rule, entity, params):
+    firewall_rule_name, firewall_rule_vnet_rg_name, firewall_rule_vnet_name, firewall_rule_subnet_name = params
+    if not firewall_rule_name:
+        msg = 'Error! Firewall rule name is missing.'
+        logging.info(f'{__file__} - {msg}')
+    elif not firewall_rule_vnet_rg_name:
+        msg = 'Error! Firewall rule VNet resource group is missing.'
+        logging.info(f'{__file__} - {msg}')
+    elif not firewall_rule_vnet_name:
+        msg = 'Error! Firewall rule VNet name is missing.'
+        logging.info(f'{__file__} - {msg}')    
+    elif not firewall_rule_subnet_name:
+        msg = 'Error! Firewall rule Subnet name is missing.'
+        logging.info(f'{__file__} - {msg}')
+
+    logging.info(f'{__file__} - ${run_action.__name__} started')
+    group_name = entity.get('resourceGroup', {}).get('name')
+    subscription_id = entity.get('accountNumber')
+    server_name = entity.get('name')
+    server_location = entity.get('region')
+    server_group_name = entity.get('resourceGroup', {}).get('name')
+    logging.info(
+        f'{__file__} - subscription_id : {subscription_id} - group_name : {group_name} - server_name : {server_name}')
+
+    if not subscription_id or not credentials:
+        return raise_credentials_error()
+
+    subnet_path = '/subscriptions/' + subscription_id + '/resourceGroups/' + firewall_rule_vnet_rg_name + \
+        '/providers/Microsoft.Network/virtualNetworks/' + \
+        firewall_rule_vnet_name + '/subnets/' + firewall_rule_subnet_name
+
+    try:
+        sql_client = SqlManagementClient(credentials, subscription_id)
+        sql_client.virtual_network_rules.begin_create_or_update(server_group_name, server_name, firewall_rule_name, subnet_path, ignore_missing_vnet_service_endpoint=False)
+        msg = f'Azure SQL firewall rule {firewall_rule_name} successfully on : {server_name}'
+        logging.info(f'{__file__} - {msg}')
+        return f'{msg}'
+
+    except HttpResponseError as e:
+        msg = f'Unexpected error : {e.message}'
+        logging.info(f'{__file__} - {msg}')
+        return msg
+    
---
+++ dome9CloudBots/bots/virtual_machine_deallocate.py
@@ -0,0 +1,27 @@
+# Last checked 13/1/21
+
+from azure.core.exceptions import HttpResponseError
+from azure.mgmt.compute import ComputeManagementClient
+import logging
+
+def run_action(credentials ,rule, entity, params):
+    logging.info(f'{__file__} - run_action started')
+    subscription_id = entity.get('accountNumber')
+    group_name = entity.get('resourceGroup',{}).get('name')
+    vm_name =entity.get('name') 
+    logging.info(f'{__file__} - subscription_id : {subscription_id} - group_name : {group_name} vm_name : {vm_name}')
+    if not subscription_id or not credentials:
+        msg = 'Error! Subscription id or credentials are missing.'
+        logging.info(f'{__file__} - {msg}')
+        return f'{msg}' 
+    compute_client = ComputeManagementClient(credentials, subscription_id) 
+    try:
+        compute_client.virtual_machines.deallocate(group_name, vm_name)
+        id = entity.get('id')
+        msg = f'Virtual machine was deallocated. id: {id}'
+        logging.info(f'{__file__} - {msg}')
+        return f'{msg}'
+    except HttpResponseError as e:   
+        msg = f'Unexpected error : {e.message}'
+        logging.info(f'{__file__} - {msg}') 
+        return f'{msg}'
