196acd19169a67d9ddaca020fb40a99cade4b439
yairshp-point <73879136+yairshp-point@users.noreply.github.com>
2021-10-04 11:41:58 +0300

    fixed is_port_in_range method and looping over nics

--- dome9CloudBots/bots/modify_network_security_group_scope_by_port.py
+++ dome9CloudBots/bots/modify_network_security_group_scope_by_port.py
@@ -1,4 +1,4 @@
-# What it does: Change network security group scope by a given port.
+# What it does: Change network security group rule scope by a given port.
 # Scope can be list of ip addresses with , between. example: 192.168.99.0/24,10.0.0.0/24,44.66.0.0/24
 # Direction can be : source or destination
 # Access can be : Allow or Deny
@@ -16,6 +16,7 @@ from itertools import chain
 
 ONE_SCOPE = 1
 ONE_PORT = 1
+SEPARATOR = '-'
 
 
 def is_port_range(port):
@@ -38,13 +39,12 @@ def is_port_in_range(port_to_find, ports_list):
         if port == port_to_find:
             return True
 
-        else:
+        if SEPARATOR in port:
             # Due to Azure SDK we split cases as port can be : '360' or '360-366'
-            ports = port.split('-')
+            ports = port.split(SEPARATOR)
             starting_port, ending_port = ports
-            if is_port_range(ports) and port_to_find > starting_port and port_to_find < ending_port:
+            if is_port_range(ports) and starting_port <= port_to_find <= ending_port:
                 return True
-
     return False
 
 
@@ -58,46 +58,75 @@ def run_action(credentials, rule, entity, params):
     
     if entity_type == 'VirtualMachine':
         logging.info(f'Entity is a VM')
-        nsg_name = entity['nics'][0]['networkSecurityGroup']['name']
-        resource_group_name = entity['nics'][0]['networkSecurityGroup']['resourceGroup']['name']
+        # nsg_name = entity['nics'][0]['networkSecurityGroup']['name']
+        # resource_group_name = entity['nics'][0]['networkSecurityGroup']['resourceGroup']['name']
+
+        num_of_nics = len(entity['nics'])
+        nsg_names = [entity['nics'][i]['networkSecurityGroup']['name'] for i in range(num_of_nics)]  # get names of nics
+        resource_group_names = [entity['nics'][i]['networkSecurityGroup']['resourceGroup']['name']
+                                for i in range(num_of_nics)]  # get names of the nic resource group
+
+        nsgs_and_resource_groups = zip(nsg_names, resource_group_names)
+
     else:
         logging.info(f'Entity is an NSG')
-        resource_group_name = entity.get('resourceGroup', {}).get('name')
-        nsg_name = entity.get('name')
-    
-    logging.info(f'{__file__} - subscription_id : {subscription_id} - group_name : {resource_group_name} nsg_name : {nsg_name}')
+        resource_group_names = [entity.get('resourceGroup', {}).get('name')]
+        nsg_names = [entity.get('name')]
 
-    try:
-        network_client = NetworkManagementClient(
-            credentials,
-            subscription_id
-        )
-        nsg = network_client.network_security_groups.get(resource_group_name, nsg_name)
+        nsgs_and_resource_groups = zip(nsg_names, resource_group_names)
 
-        # Change nsg - NetworkSecurityGroup to dict
-        nsg_dict = nsg.as_dict()
-        security_rules = nsg_dict.get('security_rules')
-        for rule in security_rules:
+    output_msg = ''
 
-            if rule.get('destination_port_range'):
-                if rule.get('destination_port_range') == port:
-                    modify_scope(rule, direction, scope, access)
+    for nsg_name, resource_group_name in nsgs_and_resource_groups:
+        logging.info(f'{__file__} - subscription_id : {subscription_id} - group_name : {resource_group_name} nsg_name : {nsg_name}')
 
-            elif rule.get('destination_port_ranges'):
-                if is_port_in_range(port, rule.get('destination_port_ranges')):
-                    modify_scope(rule, direction, scope, access)
+        try:
+            network_client = NetworkManagementClient(
+                credentials,
+                subscription_id
+            )
+            nsg = network_client.network_security_groups.get(resource_group_name, nsg_name)
 
-        nsg_dict['security_rules'] = security_rules
-        nsg_after_change = nsg.from_dict(nsg_dict)
-        network_client.network_security_groups.begin_create_or_update(resource_group_name, nsg_name, nsg_after_change)
-        entity_ID = entity.get('id')
-        msg = f'Network Security group name: {nsg_name} with id: {entity_ID} was modified, port: {port} direction:{direction} scope:{scope}'
-        logging.info(f'{__file__} - {msg}')
+            # Change nsg - NetworkSecurityGroup to dict
+            modified = False
+            nsg_dict = nsg.as_dict()
+            security_rules = nsg_dict.get('security_rules')
+            for rule in security_rules:
 
-        return f'{msg}'
+                if rule.get('destination_port_range'):
+                    if rule.get('destination_port_range') == port:
+                        modify_scope(rule, direction, scope, access)
+                        modified = True
 
-    except HttpResponseError as e:
-        msg = f'unexpected error : {e.message}'
-        logging.info(f'{__file__} - {msg}')
+                elif rule.get('destination_port_ranges'):
+                    if is_port_in_range(port, rule.get('destination_port_ranges')):
+                        modify_scope(rule, direction, scope, access)
+                        modified = True
 
-        return f'{msg}'
+            if not modified:
+                continue
+
+            nsg_dict['security_rules'] = security_rules
+            nsg_after_change = nsg.from_dict(nsg_dict)
+            network_client.network_security_groups.begin_create_or_update(resource_group_name,
+                                                                          nsg_name, nsg_after_change)
+            entity_id = entity.get('id')
+            log_msg = f'Network Security group name: {nsg_name} with id: {entity_id} was modified, port: {port} ' \
+                      f'direction:{direction} scope:{scope}\n'
+            logging.info(f'{__file__} - {log_msg}')
+            output_msg += log_msg
+
+        except HttpResponseError as e:
+            log_msg = f'Request to Azure failed on network security group: {nsg_name} -\n {e.message}'
+            logging.info(f'{__file__} - {log_msg}')
+            output_msg += log_msg
+
+        except Exception as e:
+            log_msg = f'Unexpected exception: {e}'
+            logging.info(f'{__file__} - {log_msg}')
+            output_msg += log_msg
+            return output_msg
+
+    return output_msg
+
+
